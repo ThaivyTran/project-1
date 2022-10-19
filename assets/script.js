5d40d6bc0b2d729b3c34ca26f62c0f34dbabc742
unknown <jessebeitlercode@gmail.com>
2022-10-19 19:59:28 -0400

    fixing map location

--- assets/script.js
+++ assets/script.js
@@ -34,3 +34,25 @@ wednesdaybutton.addEventListener("click", function(){
    console.log(todayslocation)
    L.marker([39.967285, -75.172344]).addTo(map);
 })
+
+
+
+
+
+
+
+
+// <!-- <script>var map = L.map('map').setView([39.952583,-75.165222], 13);</script>
+// --  refference for javascript map code--
+// <script>L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// maxZoom: 15,
+// attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);</script>
+// <script>L.marker([39.924827,-75.172945]).addTo(map);</script>
+// <script>L.marker([39.919390, -75.165419]).addTo(map);</script> -->
+
+// <!-- <script>var map = L.map('map').setView([39.952583,-75.165222], 13);</script>
+// <script>L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// maxZoom: 19,
+// attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);</script> -->
