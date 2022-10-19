32ee819506d73b9e602f57851dc2e879ae690ada
unknown <jessebeitlercode@gmail.com>
2022-10-19 17:23:59 -0400

    reworking jesse javascript to perp for map

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+<script>var map = L.map('map').setView([39.952583,-75.165222], 13);</script>
+
+<script>L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   maxZoom: 15,
+   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);</script>
