2d9d302ab8f79d28cb6ff3fa55bf2bbedd1a83da
unknown <jessebeitlercode@gmail.com>
2022-10-19 18:21:37 -0400

    working in Javascript to connect buttons to map

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+   <var id="monday"></var>;
+   <var id="Tuesday"></var>;
+   <var id="Wednesday"></var>;
+   <var id="Thursday"></var>;
+   <var id="Friday"></var>;
+   <var id="Saturday"></var>;
+   <var id="sunday"></var>;
+   <><div id="map">;
+
+   <button onClick="choose(Monday)">Monday</button>
+   <button onClick="choose('Tuesday')">Tuesday</button>
+   <button onClick="choose('Wednesday')">Wednesday</button>
+   <button onClick="choose('Thursday')">Thursday</button>
+   <button onClick="choose('Friday')">Friday</button>
+   <button onClick="choose('Saturday')">Saturday</button>
+   <button onClick="choose('sunday')">sunday</button>
+</div>
+
+   <script>
+      function myFunction()
+      var x = document.getElementById("Var");
+      {"Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, sunday"}
+   </script></>
