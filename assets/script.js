dd03bb31d5c6935e31c27ee7dcd94922d4d66dab
unknown <jessebeitlercode@gmail.com>
2022-10-19 17:34:07 -0400

    2nd attempt reworking JS to show map

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-<script>var map = L.map('map').setView([39.952583,-75.165222], 13);</script>
-
-<script>L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   maxZoom: 15,
-   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);</script>
